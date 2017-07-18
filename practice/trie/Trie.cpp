75f05858ded34fc3c3374bf34b939baaa75b27e4
Adrishya <adrishyaabhathi@gmail.com>
2017-07-18 17:35:36 +0530

    changed add functions from const to non-const

--- practice/trie/Trie.cpp
+++ practice/trie/Trie.cpp
@@ -4,11 +4,11 @@ Trie::Trie(){
     _head = std::make_shared<Node>();
 }
 
-bool Trie::add(const std::string& word_) const {
+bool Trie::add(const std::string& word_) {
     addImpl(word_,_head);
 }
 
-bool Trie::addImpl(const std::string& word_, const std::shared_ptr<Node>& currentPtr_) const{
+bool Trie::addImpl(const std::string& word_, const std::shared_ptr<Node>& currentPtr_) {
 //    shared_ptr<Node>
 }
 
